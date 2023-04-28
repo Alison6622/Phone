6955d9721e61d87e081813eac166bb90556814de
honor <apotapova250@gmail.com>
2023-04-28 21:42:59 +0300

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using PhoneApp.ViewModel;
+
+namespace PhoneApp;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void SaveClick(object sender, EventArgs e)
+	{
+		MainClass.name = NameText.Text;
+		MainClass.surname = SurnameText.Text;
+		MainClass.date = DatePic.Date;
+	}
+    private void RecoveryClick(object sender, EventArgs e)
+    {
+		NameText.Text = MainClass.name;
+		SurnameText.Text = MainClass.surname;
+		DatePic.Date = MainClass.date;
+    }
+}
+
---
+++ ViewModel/MainClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneApp.ViewModel
+{
+    class MainClass
+    {
+        public static string name;
+        public static string surname;
+        public static DateTime date;
+    }
+}
